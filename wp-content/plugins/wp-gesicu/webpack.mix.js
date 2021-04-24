9110506c27ad489295551c8c72de7a561d9b662e
Ing. Miguel Díaz Riveaux <mdriveaux2015@gmail.com>
2021-04-24 14:40:35 -0400

    Botón enviar Letras en dispositivo móvil Redes sociales pie de pagina font-size

--- wp-content/plugins/wp-gesicu/webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-
-mix.postCss('style.css', 'css/style_tailwindcss.css', [
-    tailwindcss('tailwind.config.js')
-]).options({
-    processCssUrls: false
-});
